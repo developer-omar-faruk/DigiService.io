bea43d4a3ea52441dfacf979ab8f125bc89f4d99
Developer-Omar-Faruk <mdomarfaruk1045@gmail.com>
2026-05-21 22:27:11 +0600

    Service

---
+++ src/conponents/Services.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div id='Services' className='py-20'>
+      <div className='container mx-auto px-6 sm:px-12 md:px-24 lg:px-40'>
+        <div className='text-center mb-16'>
+            <h1 className='text-4xl md:text-5xl text-dark mb-6'>How we can Help you?</h1>
+            <p className='text-dark/60 text-xl max-w-2xl mx-auto'>Whether you're launching a new business or looking to elevate your existing brand.</p>
+        </div>
+        {/* Services Data display */}
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Services
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../conponents/Hero'
 import Company from '../conponents/Company'
+import Services from '../conponents/Services'
 
 function Home() {
   return (
-    <div>
+    <div id='Home'>
       <Hero/>
       <Company/>
+      <Services/>
     </div>
   )
 }
